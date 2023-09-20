bb44d4fed37e52d3b627518e5ea5a58e37d65022
Ilya Shilov <i.o.shilov@gmail.com>
2023-09-21 01:03:54 +0600

    test

--- src/main/java/com/example/capstone/pizza_delivery_service/PizzaDeliveryServiceApplication.java
+++ src/main/java/com/example/capstone/pizza_delivery_service/PizzaDeliveryServiceApplication.java
@@ -2,12 +2,22 @@ package com.example.capstone.pizza_delivery_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PizzaDeliveryServiceApplication {
+public class PizzaDeliveryServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaDeliveryServiceApplication.class, args);
+
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<PizzaDeliveryServiceApplication> applicationClass = PizzaDeliveryServiceApplication.class;
 
 }
